5df2d2b957ef187b3fc84a20612909b01b3114bb
ARTrex007 <arturmarksz7@gmail.com>
2021-02-28 16:32:12 +0100

    Summary

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	System.out.println("Hello world!");
+	System.out.println(args);
+	System.out.println(new Date());
+    }
+}
